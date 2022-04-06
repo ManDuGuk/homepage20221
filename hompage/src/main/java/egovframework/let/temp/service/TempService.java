9fe232f2960df0f14d7735813a8cb19c6fb54427
Student <Student@DESKTOP-VD2CFFR>
2022-04-06 10:22:25 +0900

    2020.04.06 지난시간꺼완성 초기화 버전

---
+++ hompage/src/main/java/egovframework/let/temp/service/TempService.java
@@ -0,0 +1,11 @@
+package egovframework.let.temp.service;
+
+import java.util.List;
+import java.util.Map;
+
+
+public interface TempService {
+
+	public TempVO selectTemp(TempVO vo) throws Exception;
+
+}
